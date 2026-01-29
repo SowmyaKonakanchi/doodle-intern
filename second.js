3eea211118a88f2493653b2717bc74a75655a346
sowmya konakanchi <sowmyakonakanchi7@gmail.com>
2026-01-29 12:31:08 +0530

    first commit

---
+++ second.js
@@ -0,0 +1,19 @@
+function secondLargestFromString(str) {
+  let arr = str.split(" ").map(Number);
+
+  let largest = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > largest) {
+      second = largest;
+      largest = num;
+    } else if (num > second && num < largest) {
+      second = num;
+    }
+  }
+
+  return second;
+}
+
+console.log(secondLargestFromString("10 5 20 8"));
